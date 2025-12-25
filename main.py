d868d43578655c7342e4b97c0095d1942b171431
Vince Makharovich <v.makharovich@gmail.com>
2025-12-25 12:40:05 -0500

    use Tavily search to perform search in the internet with structured output

--- main.py
+++ main.py
@@ -1,27 +1,72 @@
+from typing import List
+
+from langchain_classic.agents import create_react_agent, AgentExecutor
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
-from langchain.agents import create_agent
-from langchain.tools import tool
-from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
-from tavily import TavilyClient
-from langchain_tavily import TavilySearch
 
 load_dotenv()
+from langchain_classic import hub
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import Runnable, RunnableLambda
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 
 
-llm = ChatOpenAI(model="gpt-5")
+
+
+
+
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+
+
+
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url: str = Field(..., description="The URL to the source")
+
+class AgentResponse(BaseModel):
+    """Schema for a response for agent with answer and sources"""
+    answer:str = Field(..., description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="A list of sources to generate the answer")
+
+
+
+llm = ChatOpenAI(model="gpt-4")
+structured_llm = llm.with_structured_output(AgentResponse)
 tools = [
     TavilySearch(),
 ]
-agent = create_agent(model=llm, tools=tools)
+react_prompt = hub.pull("hwchase17/react")
+#agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names", "tools"]
 
+).partial(format_instructions="")
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt_with_format_instructions,
+)
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    verbose=True,
+)
+extract_output = RunnableLambda(lambda x: x["output"])
+chain = agent_executor | extract_output | structured_llm
 
 def main():
-    print("Hello from langchain-ai-websearch!")
-    response = agent.invoke({ "messages": HumanMessage(content="What is the weather in Tokyo?")})
-    print(response)
+    result = chain.invoke(
+        input={
+            "input": "your query"
+        }
+    )
+    print(result)
+
 
 
 if __name__ == "__main__":
